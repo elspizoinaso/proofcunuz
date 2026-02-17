2b9830ce9b81289e8fe120250be70f18b09951d5
elspizoinaso <hesapcomtr295@gmail.com>
2026-02-17 23:29:41 +0300

    Create firebase.js

---
+++ proof-site/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAKYEtIsAm-9KJZo7Q7QLWHy9kBTGrOmI",
+  authDomain: "proofcunuz.firebaseapp.com",
+  projectId: "proofcunuz",
+  storageBucket: "proofcunuz.firebasestorage.app",
+  messagingSenderId: "735014919300",
+  appId: "1:735014919300:web:4a80ff283ece6cc05cf6d1"
+};
